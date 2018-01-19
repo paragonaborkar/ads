72597029c4f3a5a08845045998ead52f059edb2c
paragonaborkar <35114708+paragonaborkar@users.noreply.github.com>
2018-01-19 16:15:35 -0500

    Updated allow origin header for all URLS

--- ads-backend/src/main/java/com/netapp/ads/filter/CORSFilter.java
+++ ads-backend/src/main/java/com/netapp/ads/filter/CORSFilter.java
@@ -24,7 +24,7 @@ public class CORSFilter implements Filter{
 	@Override
 	public final void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain) throws IOException, ServletException {
 	    final HttpServletResponse response = (HttpServletResponse) res;
-	    response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+	    response.setHeader("Access-Control-Allow-Origin", "*");
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
 	    response.setHeader("Access-Control-Allow-Methods", "*");
 	    response.setHeader("Access-Control-Allow-Headers", "*");
