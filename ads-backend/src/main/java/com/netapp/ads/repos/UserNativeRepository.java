3c4d5429fd0ba3353f0e5ffcc6678e09651c4fc8
paragonaborkar <35114708+paragonaborkar@users.noreply.github.com>
2018-01-30 10:34:55 -0500

    Update UserNativeRepository.java

--- ads-backend/src/main/java/com/netapp/ads/repos/UserNativeRepository.java
+++ ads-backend/src/main/java/com/netapp/ads/repos/UserNativeRepository.java
@@ -2,14 +2,15 @@ package com.netapp.ads.repos;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.netapp.ads.models.UserNative;
 
-@RepositoryRestResource
-public interface UserNativeRepository extends PagingAndSortingRepository<UserNative, Integer>  {
+@Repository
+public interface UserNativeRepository extends JpaRepository<UserNative, Integer>, JpaSpecificationExecutor<UserNative>  {
 
 	UserNative findFirstByEmail(@Param("email") String email);
 	
