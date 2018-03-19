a7664037d4a1eec70ac8bcee1ee09f9260137ae5
paragonaborkar <35114708+paragonaborkar@users.noreply.github.com>
2018-03-19 15:02:23 -0400

    Fixed issue with Verum contacts

--- ads-backend/src/main/java/com/netapp/ads/services/ApplicationService.java
+++ ads-backend/src/main/java/com/netapp/ads/services/ApplicationService.java
@@ -1,9 +1,7 @@
 package com.netapp.ads.services;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -19,12 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.netapp.ads.models.Host;
-import com.netapp.ads.models.UserCorporate;
-import com.netapp.ads.models.verum.application.Application;
 import com.netapp.ads.models.verum.application.OSI;
-import com.netapp.ads.models.verum.application.VerumObjectList;
-import com.netapp.ads.models.verum.esource.EmployeeList;
 import com.netapp.ads.models.verum.esource.EmployeeProfile;
 import com.netapp.ads.models.verum.person.Person;
 import com.netapp.ads.repos.ApplicationRepository;
@@ -98,107 +91,6 @@ public class ApplicationService {
         return employeeProfile;
 	}
 	
-	public List<com.netapp.ads.models.Application> createApplicationAndUsers(Host host) {
-		String ipAddress = host.getIpAddr();
-		log.debug("createApplicationAndUsers(): [ENTERED]: ipAddress: " + ipAddress);
-		OSI osi = getVerumApp(ipAddress);
-		List<com.netapp.ads.models.Application> applications = new ArrayList<>();
-		if(osi != null) {
-			List<VerumObjectList> verumObjectLists = osi.getVerumObjectList();
-			for (VerumObjectList verumObjectList : verumObjectLists) {
-				List<Application> verumApplications = verumObjectList.getApplication();
-				for(Application verumApplication : verumApplications) {
-					com.netapp.ads.models.Application application = applicationRepository.findByApplicationCode(verumApplication.getAppID());
-					if(application == null) {
-						log.debug("createApplicationAndUsers(): Application DOES NOT exist. Creating.." );
-						application = new com.netapp.ads.models.Application();
-						//THERE ARE MULTIPLE CONTACTS. LOOKS LIKE OLD CODE IS PICKING ONLY THE LAST ONE. SO DID WE
-						//CAN AN APPLICATION NOT HAVE MORE THAN 1 CONTACT?
-						UserCorporate corporateUser = getOrCreateCorporateUser(verumApplication.getContact().get(0).getSid());
-						if(corporateUser != null) {
-							application.setOwnerUserCorporateId(corporateUser.getId()); //WHAT IF CORPORATE USER DOES NOT EXIST IN SYSTEM? SHOULD WE CREATE? VERUM IS SENDING MULTIPLE CONTACTS
-						}
-						
-						if(!application.getHosts().contains(host)) {
-							application.addHost(host);
-						}
-						application.setApplicationName(verumApplication.getApplicationName());
-						application.setApplicationCode(verumApplication.getAppID());
-						//application.setArchtype(verumApplication.get); WHERE TO GET THIS FROM?
-						//application.setHosts(hosts); DO WE NEED TO SET THIS?
-						application.setInformationOwner(verumApplication.getTechGroupOwner()); //IS THIS CORRECT?
-						//application.setLineOfBusinesses(new ListverumApplication.getOwningLOB()); //DO WE NEED TO SET THIS?
-						application = applicationRepository.save(application);
-						log.debug("createApplicationAndUsers(): Application Created/Updated: " + application.getId());
-					} else {
-						log.debug("createApplicationAndUsers(): Application already exists: " + application.getId());
-					}
-					applications.add(application);
-				}
-			}
-		}
-		log.debug("createApplicationAndUsers(): [EXIT]");
-		return applications;
-	}
-	
-	public UserCorporate getOrCreateCorporateUser(String sid) {
-		log.debug("getOrCreateCorporateUser: sid: " + sid);
-		UserCorporate corporateUser = null;
-		Person person = getVerumPerson(sid);
-		if(person != null && !person.getVerumObjectList().isEmpty()) {
-			List<com.netapp.ads.models.verum.person.VerumObjectList> personVerumObjectLists = person.getVerumObjectList();
-			com.netapp.ads.models.verum.person.VerumObjectList personVerumObjectList = personVerumObjectLists.get(0);
-			UserCorporate manager = userCorporateRepository.findFirstByUserName(personVerumObjectList.getManagerSid());
-			if(manager == null) {
-				log.debug("getOrCreateCorporateUser(): Manager does not exist");
-				EmployeeProfile managerProfile = getEmployeeProfile(personVerumObjectList.getManagerSid());
-				if(managerProfile != null && !managerProfile.getEmployeeList().isEmpty()) {
-					EmployeeList managerEmployee = managerProfile.getEmployeeList().get(0);
-					manager = new UserCorporate();
-					manager.setUserName(personVerumObjectList.getManagerSid());
-					manager.setFirstName(managerEmployee.getFirstName());
-					manager.setLastName(managerEmployee.getLastName());
-					manager.setMiddleName(managerEmployee.getMiddle());
-					manager.setEmail(managerEmployee.getIAddress());
-					manager.setBestPhone(managerEmployee.getPhoneNo());
-					manager.setMobilePhone(managerEmployee.getCellPhoneNo());
-					manager.setTitle(managerEmployee.getTitle());
-					manager.setLocation(managerEmployee.getBuildStreetAddress() + "," + managerEmployee.getBuildState() + "," + managerEmployee.getBuildZip() + "," + managerEmployee.getBuildCountry());
-					userCorporateRepository.save(manager);
-				}
-			}
-
-			corporateUser = userCorporateRepository.findFirstByUserName(sid);
-			if(corporateUser == null) {
-				log.debug("getOrCreateCorporateUser(): Corporate user DOES NOT exist. Creating....");
-				corporateUser = new UserCorporate();
-				corporateUser.setCostCenter(personVerumObjectList.getCostCenter());
-				corporateUser.setUserName(sid);
-				corporateUser.setUserCorporateManager(manager);
-				
-				EmployeeProfile employeeProfile = getEmployeeProfile(sid);
-				if(employeeProfile != null && !employeeProfile.getEmployeeList().isEmpty()) {
-					//We are getting a targeted user here based on SID so this list should always have 1 value
-					EmployeeList employee = employeeProfile.getEmployeeList().get(0);
-					corporateUser.setFirstName(employee.getFirstName());
-					corporateUser.setLastName(employee.getLastName());
-					corporateUser.setMiddleName(employee.getMiddle());
-					corporateUser.setEmail(employee.getIAddress());
-					corporateUser.setBestPhone(employee.getPhoneNo());
-					corporateUser.setMobilePhone(employee.getCellPhoneNo());
-					corporateUser.setTitle(employee.getTitle());
-					corporateUser.setLocation(employee.getBuildStreetAddress() + "," + employee.getBuildState() + "," + employee.getBuildZip() + "," + employee.getBuildCountry());
-					corporateUser = userCorporateRepository.save(corporateUser);
-					log.debug("getOrCreateCorporateUser(): Created/Updated Corporate User: " + corporateUser.getId());
-				} //end if
-			} else {
-				log.debug("getOrCreateCorporateUser(): Corporate user already exists: " + corporateUser.getId());
-			}
-		}
-		return corporateUser;
-	}
-	
-
 	public HttpHeaders getDefaultHttpHeaders() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.TEXT_PLAIN);
--- ads-backend/src/main/java/com/netapp/ads/services/OwnerIdentificationService.java
+++ ads-backend/src/main/java/com/netapp/ads/services/OwnerIdentificationService.java
@@ -1,5 +1,6 @@
 package com.netapp.ads.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,15 +11,23 @@ import org.springframework.stereotype.Service;
 
 import com.netapp.ads.models.Activity;
 import com.netapp.ads.models.ActivityResponse;
-import com.netapp.ads.models.Application;
 import com.netapp.ads.models.Host;
 import com.netapp.ads.models.Qtree;
 import com.netapp.ads.models.QtreeDisposition;
 import com.netapp.ads.models.Share;
+import com.netapp.ads.models.UserCorporate;
+import com.netapp.ads.models.verum.application.Contact;
+import com.netapp.ads.models.verum.application.OSI;
+import com.netapp.ads.models.verum.application.VerumObjectList;
+import com.netapp.ads.models.verum.esource.EmployeeList;
+import com.netapp.ads.models.verum.esource.EmployeeProfile;
+import com.netapp.ads.models.verum.person.Person;
 import com.netapp.ads.repos.ActivityRepository;
 import com.netapp.ads.repos.ActivityResponseRepository;
+import com.netapp.ads.repos.ApplicationRepository;
 import com.netapp.ads.repos.HostRepository;
 import com.netapp.ads.repos.QtreeDispositionRepository;
+import com.netapp.ads.repos.UserCorporateRepository;
 
 @Service
 public class OwnerIdentificationService {
@@ -42,6 +51,12 @@ public class OwnerIdentificationService {
 	
 	@Autowired
 	QtreeDispositionRepository qtreeDispositionRepository;
+	
+	@Autowired
+	public ApplicationRepository applicationRepository;
+	
+	@Autowired
+	public UserCorporateRepository userCorporateRepository;	
 	
 	/**
 	 * Finds activities where owner needs to be discovered
@@ -81,11 +96,7 @@ public class OwnerIdentificationService {
 			
 			//Based on the hosts IP Addresses call CMDB to fetch applications and corresponding corporate owner users
 			//then for each corporate user create an activity response which will assist in owner identification
-			List<Application> applications = applicationService.createApplicationAndUsers(host);
-			for(Application application: applications) {
-				List<ActivityResponse> activityResponses = activity.getActivityResponses();
-				createActivityResponse(activity, activityResponses, application.getOwnerUserCorporateId());
-			} //end of for loop for applications
+			createApplicationUsersAndActivityResponses(host, activity);
 			
 			updateHostAsPerActivity(host, activity);
 		} //end if for host check 
@@ -109,6 +120,115 @@ public class OwnerIdentificationService {
 			} // else
 		} //main else for host
 	}
+	
+	public List<com.netapp.ads.models.Application> createApplicationUsersAndActivityResponses(Host host, Activity activity) {
+		String ipAddress = host.getIpAddr();
+		log.debug("createApplicationAndUsers(): [ENTERED]: ipAddress: " + ipAddress);
+		OSI osi = applicationService.getVerumApp(ipAddress);
+		List<com.netapp.ads.models.Application> applications = new ArrayList<>();
+		if(osi != null) {
+			List<VerumObjectList> verumObjectLists = osi.getVerumObjectList();
+			for (VerumObjectList verumObjectList : verumObjectLists) {
+				List<com.netapp.ads.models.verum.application.Application> verumApplications = verumObjectList.getApplication();
+				for(com.netapp.ads.models.verum.application.Application verumApplication : verumApplications) {
+					com.netapp.ads.models.Application application = applicationRepository.findByApplicationCode(verumApplication.getAppID());
+					if(application == null) {
+						log.debug("createApplicationAndUsers(): Application DOES NOT exist. Creating.." );
+						List<ActivityResponse> activityResponses = activity.getActivityResponses();
+						//THERE ARE MULTIPLE CONTACTS. LOOKS LIKE OLD CODE IS PICKING ONLY THE LAST ONE. SO DID WE
+						//CAN AN APPLICATION NOT HAVE MORE THAN 1 CONTACT?
+						Integer corpOwnerId = 0;
+						List<Contact> contacts = verumApplication.getContact();
+						for(Contact contact: contacts) {
+							UserCorporate corporateUser = getOrCreateCorporateUser(contact.getSid());
+							if(corporateUser != null) {
+								//application.setOwnerUserCorporateId(corporateUser.getId()); //WHAT IF CORPORATE USER DOES NOT EXIST IN SYSTEM? SHOULD WE CREATE? VERUM IS SENDING MULTIPLE CONTACTS
+								createActivityResponse(activity, activityResponses, corporateUser.getId());
+								corpOwnerId = corporateUser.getId();
+							}
+						}
+
+						application = new com.netapp.ads.models.Application();
+						if(!application.getHosts().contains(host)) {
+							application.addHost(host);
+						}
+						application.setApplicationName(verumApplication.getApplicationName());
+						application.setApplicationCode(verumApplication.getAppID());
+						application.setOwnerUserCorporateId(corpOwnerId);
+						//application.setArchtype(verumApplication.get); WHERE TO GET THIS FROM?
+						//application.setHosts(hosts); DO WE NEED TO SET THIS?
+						application.setInformationOwner(verumApplication.getTechGroupOwner()); //IS THIS CORRECT?
+						//application.setLineOfBusinesses(new ListverumApplication.getOwningLOB()); //DO WE NEED TO SET THIS?
+						application = applicationRepository.save(application);
+						
+						log.debug("createApplicationAndUsers(): Application Created/Updated: " + application.getId());
+					} else {
+						log.debug("createApplicationAndUsers(): Application already exists: " + application.getId());
+					}
+					applications.add(application);
+				}
+			}
+		}
+		log.debug("createApplicationAndUsers(): [EXIT]");
+		return applications;
+	}
+	
+	public UserCorporate getOrCreateCorporateUser(String sid) {
+		log.debug("getOrCreateCorporateUser: sid: " + sid);
+		UserCorporate corporateUser = null;
+		Person person = applicationService.getVerumPerson(sid);
+		if(person != null && !person.getVerumObjectList().isEmpty()) {
+			List<com.netapp.ads.models.verum.person.VerumObjectList> personVerumObjectLists = person.getVerumObjectList();
+			com.netapp.ads.models.verum.person.VerumObjectList personVerumObjectList = personVerumObjectLists.get(0);
+			UserCorporate manager = userCorporateRepository.findFirstByUserName(personVerumObjectList.getManagerSid());
+			if(manager == null) {
+				log.debug("getOrCreateCorporateUser(): Manager does not exist");
+				EmployeeProfile managerProfile = applicationService.getEmployeeProfile(personVerumObjectList.getManagerSid());
+				if(managerProfile != null && !managerProfile.getEmployeeList().isEmpty()) {
+					EmployeeList managerEmployee = managerProfile.getEmployeeList().get(0);
+					manager = new UserCorporate();
+					manager.setUserName(personVerumObjectList.getManagerSid());
+					manager.setFirstName(managerEmployee.getFirstName());
+					manager.setLastName(managerEmployee.getLastName());
+					manager.setMiddleName(managerEmployee.getMiddle());
+					manager.setEmail(managerEmployee.getIAddress());
+					manager.setBestPhone(managerEmployee.getPhoneNo());
+					manager.setMobilePhone(managerEmployee.getCellPhoneNo());
+					manager.setTitle(managerEmployee.getTitle());
+					manager.setLocation(managerEmployee.getBuildStreetAddress() + "," + managerEmployee.getBuildState() + "," + managerEmployee.getBuildZip() + "," + managerEmployee.getBuildCountry());
+					userCorporateRepository.save(manager);
+				}
+			}
+
+			corporateUser = userCorporateRepository.findFirstByUserName(sid);
+			if(corporateUser == null) {
+				log.debug("getOrCreateCorporateUser(): Corporate user DOES NOT exist. Creating....");
+				corporateUser = new UserCorporate();
+				corporateUser.setCostCenter(personVerumObjectList.getCostCenter());
+				corporateUser.setUserName(sid);
+				corporateUser.setUserCorporateManager(manager);
+				
+				EmployeeProfile employeeProfile = applicationService.getEmployeeProfile(sid);
+				if(employeeProfile != null && !employeeProfile.getEmployeeList().isEmpty()) {
+					//We are getting a targeted user here based on SID so this list should always have 1 value
+					EmployeeList employee = employeeProfile.getEmployeeList().get(0);
+					corporateUser.setFirstName(employee.getFirstName());
+					corporateUser.setLastName(employee.getLastName());
+					corporateUser.setMiddleName(employee.getMiddle());
+					corporateUser.setEmail(employee.getIAddress());
+					corporateUser.setBestPhone(employee.getPhoneNo());
+					corporateUser.setMobilePhone(employee.getCellPhoneNo());
+					corporateUser.setTitle(employee.getTitle());
+					corporateUser.setLocation(employee.getBuildStreetAddress() + "," + employee.getBuildState() + "," + employee.getBuildZip() + "," + employee.getBuildCountry());
+					corporateUser = userCorporateRepository.save(corporateUser);
+					log.debug("getOrCreateCorporateUser(): Created/Updated Corporate User: " + corporateUser.getId());
+				} //end if
+			} else {
+				log.debug("getOrCreateCorporateUser(): Corporate user already exists: " + corporateUser.getId());
+			}
+		}
+		return corporateUser;
+	}	
 	
 	public boolean isIdEmpty(Integer id) {
 		if(id == null || id.intValue() == 0)
