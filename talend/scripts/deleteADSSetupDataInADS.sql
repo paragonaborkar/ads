a796465f8e030a427c89e1d42d236babfc1100c0
Mohana R Bondada <mohana.bondada@cgius.com>
2018-04-10 09:41:15 -0400

    fixed talend Job Bugs

--- talend/scripts/deleteADSSetupDataInADS.sql
+++
@@ -1,8 +0,0 @@
-delete from user_roles;
-delete from user_native;
-delete from audit_event;
-delete from audit_reasoncode;
-delete from application;
-delete from sysconfig;
-delete from mst_email_type;
-delete from schedules;
