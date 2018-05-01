13ecfacd9847113aefff010de900eb3076482f80
Ashish B <ashish_dt@yahoo.com>
2018-05-01 09:40:05 -0400

    selenium fixes

--- NetApp_Selenium/src/main/java/com/netapp/ads/admin/OperationalOverride.java
+++ NetApp_Selenium/src/main/java/com/netapp/ads/admin/OperationalOverride.java
@@ -2,6 +2,8 @@ package com.netapp.ads.admin;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,17 +13,21 @@ public class OperationalOverride {
 	
 	public void pageReach(WebDriver driver) {
 		driver.navigate().refresh();
-		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.id("adminDropdown"))).click();
-		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.id("admin-op-override"))).click();
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("adminDropdown"))).click();
+		new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("admin-op-override"))).click();
 	}
 	
 	public void operationalOverride(WebDriver driver) {
-		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.id("btnReset"))).click();
-		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.id("requestedBy"))).sendKeys("SELENIUM-TEST");
-		WebElement selectReasonCode = driver.findElement(By.xpath("//app-op-override-confirm//select[@formcontrolname='reasonCode']"));
-		selectReasonCode.sendKeys(Keys.ARROW_DOWN);
-		selectReasonCode.sendKeys(Keys.ENTER);
-		driver.findElement(By.id("reason")).sendKeys("SELENIUM-TEST");
-		driver.findElement(By.id("btnResubmit")).click();
+		try {
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("btnReset"))).click();
+			new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.id("requestedBy"))).sendKeys("SELENIUM-TEST");
+			WebElement selectReasonCode = driver.findElement(By.xpath("//app-op-override-confirm//select[@formcontrolname='reasonCode']"));
+			selectReasonCode.sendKeys(Keys.ARROW_DOWN);
+			selectReasonCode.sendKeys(Keys.ENTER);
+			driver.findElement(By.id("reason")).sendKeys("SELENIUM-TEST");
+			driver.findElement(By.id("btnResubmit")).click();
+		} catch(TimeoutException e) {
+			
+		}
 	}
 }
